14b7e89c82b69d8995400ecac1e2445d90c7dc78
meche234 <vameche1@terpmail.umd.edu>
2022-12-02 18:12:20 -0500

    finshed lab 8

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -82,6 +82,34 @@ function filterList(array, filterInputValue) {
   });
 }
 
+function initMap() {
+  console.log('initMap');
+  const map = L.map('map').setView([38.9897, -76.9378], 12);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  return map;
+}
+
+function makerPlace(array, map) {
+  console.log('markerPlace', array);
+  // const marker = L.marker([51.5, -0.09]).addTo(map);
+
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+  array.forEach((item, index) => {
+    const {coordinates} = item.geocoded_column_1;
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    if (index === 0) {
+      map.setView([coordinates[1], coordinates[0]], 10);
+    }
+  });
+}
+
 async function mainEvent() {
   /*
           ## Main Event
@@ -91,6 +119,7 @@ async function mainEvent() {
         */
 
   // the async keyword means we can make API requests
+  const pageMap = initMap();
   const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
   const submit = document.querySelector('#get-resto'); // get a reference to your submit button
   const loadAnimation = document.querySelector('.lds-ellipsis');
@@ -133,6 +162,7 @@ async function mainEvent() {
       console.log('input', event.target.value);
       const filteredList = filterList(currentList, event.target.value);
       injectHTML(filteredList);
+      makerPlace(filteredList, pageMap);
     });
 
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
@@ -146,6 +176,7 @@ async function mainEvent() {
 
       // And this function call will perform the "side effect" of injecting the HTML list for you
       injectHTML(currentList);
+      makerPlace(currentList, pageMap);
 
       // By separating the functions, we open the possibility of regenerating the list
       // without having to retrieve fresh data every time
